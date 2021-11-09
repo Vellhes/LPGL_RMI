ba73ebf7c28ac114c80122c4be3a05c8f557c210
Vellhes <61982987+Vellhes@users.noreply.github.com>
2021-11-09 09:48:24 +0100

    Add files via upload

---
+++ src/magasin/CommandeInterface.java
@@ -0,0 +1,5 @@
+package magasin;
+
+public interface CommandeInterface {
+
+}
---
+++ src/rmi/Client.java
@@ -0,0 +1,16 @@
+package rmi;
+
+public class Client {
+	
+	public void lancementClient() {
+		try {
+			String host = "127.0.0.1";
+			int port = 1337;
+			
+		}
+		catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+}
