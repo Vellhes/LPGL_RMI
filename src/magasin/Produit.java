525ab102f1f6276df79f0f07263bea273dd07d6e
Vellhes <61982987+Vellhes@users.noreply.github.com>
2021-11-30 10:33:07 +0100

    Update Produit.java

--- src/magasin/Produit.java
+++ src/magasin/Produit.java
@@ -2,4 +2,54 @@ package magasin;
 
 public class Produit {
 
+	private int IdProduit;
+	private String NomProduit;
+	private String DescriptionProduit;
+	private double Tarif;
+	
+	public int getIdProduit() {
+		return IdProduit;
+	}
+	
+	public void setIdProduit(int idProduit) {
+		IdProduit = idProduit;
+	}
+	
+	public String getNomProduit() {
+		return NomProduit;
+	}
+	
+	public void setNomProduit(String nomProduit) {
+		NomProduit = nomProduit;
+	}
+	
+	public String getDescriptionProduit() {
+		return DescriptionProduit;
+	}
+	
+	public void setDescriptionProduit(String descriptionProduit) {
+		DescriptionProduit = descriptionProduit;
+	}
+	
+	@Override
+	public String toString() {
+		return "Produit [IdProduit=" + IdProduit + ", NomProduit=" + NomProduit + ", DescriptionProduit="
+				+ DescriptionProduit + ", Tarif=" + Tarif + "]";
+	}
+	
+	public double getTarif() {
+		return Tarif;
+	}
+	
+	public void setTarif(float tarif) {
+		Tarif = tarif;
+	}
+	
+	public Produit(int idProduit, String nomProduit, String descriptionProduit, double tarif) {
+		super();
+		IdProduit = idProduit;
+		NomProduit = nomProduit;
+		DescriptionProduit = descriptionProduit;
+		Tarif = tarif;
+	}
 }
