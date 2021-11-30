e259175b6f04ac4cf4d2ccf6bcaaf52ca6d85f32
Vellhes <61982987+Vellhes@users.noreply.github.com>
2021-11-30 10:32:49 +0100

    Update Magasin.java

--- src/magasin/Magasin.java
+++ src/magasin/Magasin.java
@@ -2,4 +2,39 @@ package magasin;
 
 public class Magasin {
 
+	private String Adresse;
+	private String Nom;
+	private int Identifiant;
+	
+	
+	public String getAdresse() {
+		return Adresse;
+	}
+	public void setAdresse(String adresse) {
+		Adresse = adresse;
+	}
+	public String getNom() {
+		return Nom;
+	}
+	public void setNom(String nom) {
+		Nom = nom;
+	}
+	public int getIdentifiant() {
+		return Identifiant;
+	}
+	public void setIdentifiant(int identifiant) {
+		this.Identifiant = identifiant;
+	}
+	@Override
+	public String toString() {
+		return "Magasin [Adresse=" + Adresse + ", Nom=" + Nom + ", identifiant=" + Identifiant + "]";
+	}
+	public Magasin(String adresse, String nom, int identifiant) {
+		super();
+		Adresse = adresse;
+		Nom = nom;
+		this.Identifiant = identifiant;
+	}
+
+	
 }
